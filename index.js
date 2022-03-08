c469b0af217957a0ef74d80556af9704f87dff99
singhalharsh10 <singhalharsh23@gmail.com>
2022-03-08 21:52:18 +0530

    Report Generator

---
+++ index.js
@@ -0,0 +1,18 @@
+var reporter = require('cucumber-html-reporter');
+
+var options = {
+    theme: 'bootstrap',
+    jsonFile: 'report/cucumber_report.json',
+    output: 'report/cucumber_report.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true,
+    metadata: {
+        "App Version": "0.3.2",
+        "Test Environment": "STAGING",
+        "Parallel": "Scenarios",
+        "Executed": "Remote"
+    }
+};
+
+reporter.generate(options);
